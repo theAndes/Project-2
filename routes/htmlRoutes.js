c5aa4889e496d823b05e7de2757d5fceb7031b23
Mark Reed <markdreed512@gmail.com>
2019-04-04 09:59:23 -0500

    ajax call to weather and '/' route render Weather

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,19 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    console.log("db.example: ", db.Example);
+    res.render("index");
   });
+  // Load index page
+  // app.get("/", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.render("index", {
+  //       msg: "Welcome!",
+  //       examples: dbExamples
+  //     });
+  //   });
+  // });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
