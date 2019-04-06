8ed571503d7ea96ecc66a6949f729edfb610ee51
Mark Reed <markdreed512@gmail.com>
2019-04-06 12:17:30 -0500

    committing before checking out travel branch

---
+++ public/js/weatherapi.js
@@ -0,0 +1,41 @@
+function getWeather() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(queryApi);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+}
+function queryApi(position) {
+  var lat = position.coords.latitude;
+  var long = position.coords.longitude;
+  // eslint-disable-next-line prettier/prettier
+  var queryUrl = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/c862563a11d6e79b149ee1ac7e419121/"+ lat + "," + long;
+  $.ajax({
+    url: queryUrl,
+    type: "GET"
+  }).then(function(response) {
+    console.log(response);
+    var summary = response.daily.data[0].summary;
+    var highTemp = Math.round(response.daily.data[0].apparentTemperatureHigh);
+    var lowTemp = Math.round(response.daily.data[0].apparentTemperatureLow);
+    var precipType = response.daily.data[0].precipType;
+    var precipProb =
+      Math.round(response.daily.data[0].precipProbability) * 100 +
+      "% chance of " +
+      precipType;
+    $("#demo").html(
+      summary +
+        "<br>" +
+        "High: " +
+        highTemp +
+        "&#176" +
+        "<br>" +
+        "Low: " +
+        lowTemp +
+        "&#176" +
+        "<br>" +
+        precipProb
+    );
+  });
+}
+getWeather();
