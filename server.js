3b1b62fa6138a52256e84eabd9a0ea20ba045d4f
Carly Seitz <carlyseitz817@gmail.com>
2019-04-12 22:37:04 -0500

    added estimated travel time and directions link to HTML

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var exphbs = require("express-handlebars");
 var db = require("./models");
 
 var app = express();
-var PORT = process.env.PORT || 3030;
+var PORT = process.env.PORT || 3032;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
